249721e690d1c8f2a977551b1f692351c70b9c86
Gyan Vardhan <gyansingh1997@gmail.com>
2020-04-02 16:05:10 +0530

    aws deployment prep with CI

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          laura edit kare
+          Docker things setup done main development begins
         </p>
         <a
           className="App-link"
